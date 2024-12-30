6123c21f337457da5fe1a0ff939ab041f8144fc1
RealApexMan <realapexman@gmail.com>
2024-12-30 11:11:48 +0100

    Sprint 0 (#3)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'apex-plannerx' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('apex-plannerx');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,/*inject*/ } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+/*import { AuthenticationService } from './core/authentication.service';
+import { switchMap } from 'rxjs';*/
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'apex-plannerx';
+
+  /*readonly #authenticationService = inject(AuthenticationService);
+
+  onLogin(){
+    const email = 'john.doe3@gmail.com';
+    const password = 'xxxxxx';
+
+    this.#authenticationService.login(email, password).pipe(
+      switchMap((response) => {
+        const {email, localId, idToken} = response;
+        return this.#authenticationService.save(email, localId, idToken);
+      })
+    )
+    .subscribe((response) => console.log(response));
+  }
+*/
 }
