0fb7b1fc3e31fee881fd6e8c010adef71f1522eb
RealApexMan <realapexman@gmail.com>
2024-12-31 11:23:45 +0100

    Adding homepage component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component,/*inject*/ } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-/*import { AuthenticationService } from './core/authentication.service';
-import { switchMap } from 'rxjs';*/
+
 
 @Component({
   selector: 'app-root',
@@ -12,19 +11,4 @@ import { switchMap } from 'rxjs';*/
 })
 export class AppComponent {
 
-  /*readonly #authenticationService = inject(AuthenticationService);
-
-  onLogin(){
-    const email = 'john.doe3@gmail.com';
-    const password = 'xxxxxx';
-
-    this.#authenticationService.login(email, password).pipe(
-      switchMap((response) => {
-        const {email, localId, idToken} = response;
-        return this.#authenticationService.save(email, localId, idToken);
-      })
-    )
-    .subscribe((response) => console.log(response));
-  }
-*/
 }
