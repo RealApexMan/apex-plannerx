82b7e00e0187790bc2e31f98662317dd5739da96
RealApexMan <realapexman@gmail.com>
2025-02-06 19:17:43 +0100

    Updating homepage and navbar

--- src/app/core/navbar/navbar.smart.component.spec.ts
+++ src/app/core/navbar/navbar.smart.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarSmartComponent } from './navbar.smart.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('NavbarSmartComponent', () => {
   let component: NavbarSmartComponent;
   let fixture: ComponentFixture<NavbarSmartComponent>;
+
+  let title: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,7 +21,15 @@ describe('NavbarSmartComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    title = fixture.debugElement.query(By.css('[data-testid="navbar-title"]'));
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display webapp title', () => {
+    expect(title.nativeElement.textContent).toContain('Apex Planner');
   });
 });
