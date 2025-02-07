5adbc3781070ecbb4280b8d2ba438a985e28837d
RealApexMan <realapexman@gmail.com>
2025-02-07 16:57:25 +0100

    Merge angular 19 (#7)

---
+++ src/app/visitor/signup/signup.page.component.ts
@@ -0,0 +1,24 @@
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+    imports: [FormsModule],
+    templateUrl: './signup.page.component.html',
+    styleUrl: './signup.page.component.scss'
+})
+export class SignupPageComponent {
+
+  readonly name = signal('');
+  readonly email = signal('');
+  readonly password = signal('');
+  readonly confirmPassword = signal('');
+
+  readonly isPasswordMatchValid = computed(
+    () => this.password() === this.confirmPassword()
+  );
+
+  onSubmit() {
+    console.log('Form submitted')
+  }
+
+}
