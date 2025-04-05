c00c6e41eb8aac10face85cded95f6f4f5b1379b
RealApexMan <realapexman@gmail.com>
2025-04-05 17:02:15 +0200

    Sprint 1 (#8)
    Add signup feature, add signal library and refactoring some codes

--- src/app/visitor/signup/signup.page.component.ts
+++ src/app/visitor/signup/signup.page.component.ts
@@ -1,24 +1,34 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { UserStore } from '../../core/store/user.store';
+import { AuthenticationService } from '../../core/port/authentication.service';
+import { Visitor } from '../../core/entity/user.interface';
 
 @Component({
-    imports: [FormsModule],
-    templateUrl: './signup.page.component.html',
-    styleUrl: './signup.page.component.scss'
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './signup.page.component.html',
+  styleUrl: './signup.page.component.scss',
 })
 export class SignupPageComponent {
+  readonly store = inject(UserStore);
+  readonly authenticationService = inject(AuthenticationService);
 
   readonly name = signal('');
   readonly email = signal('');
   readonly password = signal('');
   readonly confirmPassword = signal('');
 
-  readonly isPasswordMatchValid = computed(
+  readonly isPasswordMatch = computed(
     () => this.password() === this.confirmPassword()
   );
 
   onSubmit() {
-    console.log('Form submitted')
+    const visitor: Visitor = {
+      name: this.name(),
+      email: this.email(),
+      password: this.password(),
+    };
+    this.store.register(visitor);
   }
-
 }
