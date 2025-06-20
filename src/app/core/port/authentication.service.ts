c2b515cb4249482936e17edf09aece61bcb19522
RealApexMan <realapexman@gmail.com>
2025-06-20 17:02:08 +0200

    Master merge resolu (#11)

--- src/app/core/port/authentication.service.ts
+++ src/app/core/port/authentication.service.ts
@@ -1,31 +1,35 @@
 import { Injectable } from '@angular/core';
- import { Observable } from 'rxjs';
- import { AuthenticationFirebaseService } from '../adapter/authentication-firebase.service';
- 
- export interface RegisterResponse {
-   jwtToken: string;
-   jwtRefreshToken: string;
-   expiresIn: string;
-   userId: string;
- }
- 
- export interface LoginResponse {
-   jwtToken: string;
-   jwtRefreshToken: string;
-   expiresIn: string;
-   userId: string;
-   isRegistered: boolean;
- }
- 
- @Injectable({
-   providedIn: 'root',
-   useClass: AuthenticationFirebaseService,
- })
- export abstract class AuthenticationService {
-   abstract register(
-     email: string,
-     password: string
-   ): Observable<RegisterResponse>;
- 
-   abstract login(email: string, password: string): Observable<LoginResponse>;
- }
+import { Observable } from 'rxjs';
+import { AuthenticationFirebaseService } from '../adapter/authentication-firebase.service';
+import { EmailAlreadyTakenError } from '@app/visitor/signup/domain/email-already-taken.error';
+
+export type RegisterResponse = RegisterPayload | EmailAlreadyTakenError;
+export type LoginResponse = LoginPayload;
+
+interface RegisterPayload {
+  jwtToken: string;
+  jwtRefreshToken: string;
+  expiresIn: string;
+  userId: string;
+}
+
+interface LoginPayload {
+  jwtToken: string;
+  jwtRefreshToken: string;
+  expiresIn: string;
+  userId: string;
+  isRegistered: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+  useClass: AuthenticationFirebaseService,
+})
+export abstract class AuthenticationService {
+  abstract register(
+    email: string,
+    password: string
+  ): Observable<RegisterResponse>;
+
+  abstract login(email: string, password: string): Observable<LoginResponse>;
+}
