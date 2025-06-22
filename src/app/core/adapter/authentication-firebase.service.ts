27973a09adc7aa82781d952b1c47b294f78fd214
RealApexMan <realapexman@gmail.com>
2025-06-22 20:37:08 +0200

    Revert "Merge branch 'master' of https://github.com/RealApexMan/apex-plannerx"
    This reverts commit 49245e3789a80991b38655f04ec052ae51c061ec, reversing
    changes made to 878734103b6c17c4310a1619164604854be2b77e.

--- src/app/core/adapter/authentication-firebase.service.ts
+++ src/app/core/adapter/authentication-firebase.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { catchError, map, Observable, of, throwError } from 'rxjs';
 import {
@@ -8,13 +8,14 @@ import {
 } from '../port/authentication.service';
 import { environment } from '@env/environment';
 import { EmailAlreadyTakenError } from '@app/visitor/signup/domain/email-already-taken.error';
+import { InvalidCredentialError } from '@app/visitor/login/domain/invalid-credential.error';
 
 /**
  * Represents the payload of the response received when registering a new user in Firebase.
  *
  * @see https://firebase.google.com/docs/reference/rest/auth?hl=fr#section-create-email-password
  */
-interface FirebaseResponseSignup {
+interface FirebaseResponseSignupPayload {
   idToken: string;
   email: string;
   refreshToken: string;
@@ -22,7 +23,7 @@ interface FirebaseResponseSignup {
   localId: string;
 }
 
-interface FirebaseResponseSignin {
+interface FirebaseResponseSigninPayload {
   displayName: string;
   email: string;
   expiresIn: string;
@@ -30,6 +31,15 @@ interface FirebaseResponseSignin {
   localId: string;
   refreshToken: string;
   registered: boolean;
+}
+
+export interface FirebaseRefreshTokenPayload {
+  expires_in: string;
+  token_type: string; // always "Bearer"
+  refresh_token: string;
+  id_token: string;
+  user_id: string;
+  project_id: string;
 }
 
 @Injectable()
@@ -40,7 +50,7 @@ export class AuthenticationFirebaseService implements AuthenticationService {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebase.apiKey}`;
     const body = { email, password, returnSecureToken: true };
 
-    return this.#http.post<FirebaseResponseSignup>(url, body).pipe(
+    return this.#http.post<FirebaseResponseSignupPayload>(url, body).pipe(
       map((response) => ({
         jwtToken: response.idToken,
         jwtRefreshToken: response.refreshToken,
@@ -61,14 +71,34 @@ export class AuthenticationFirebaseService implements AuthenticationService {
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebase.apiKey}`;
     const body = { email, password, returnSecureToken: true };
 
-    return this.#http.post<FirebaseResponseSignin>(url, body).pipe(
+    return this.#http.post<FirebaseResponseSigninPayload>(url, body).pipe(
       map((response) => ({
         jwtToken: response.idToken,
         jwtRefreshToken: response.refreshToken,
         expiresIn: response.expiresIn,
         userId: response.localId,
         isRegistered: response.registered,
-      }))
+      })),
+      catchError(error => {
+        if(error.error.error.message === 'INVALID_LOGIN_CREDENTIALS') {
+          return of(new InvalidCredentialError());
+        }
+
+        return throwError(() => error);
+      })
+    );
+  }
+    refreshToken(refreshToken: string): Observable<{ jwtToken: string, userId: string }> {
+    const url = `https://securetoken.googleapis.com/v1/token?key=${environment.firebase.apiKey}`;
+    const body = new HttpParams()
+      .set('grant_type', 'refresh_token')
+      .set('refresh_token', refreshToken)
+      .toString();
+  
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+  
+    return this.#http.post<FirebaseRefreshTokenPayload>(url, body, { headers }).pipe(
+      map(response => ({ jwtToken: response.id_token, userId: response.user_id })),
     );
   }
 }
