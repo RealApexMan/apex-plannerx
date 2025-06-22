a6d3e54f82a5eed2080fbd3186cea15aab144ac5
RealApexMan <realapexman@gmail.com>
2025-06-22 20:39:14 +0200

    Auto connect fix 2 (#14)

--- src/app/core/port/authentication.service.ts
+++ src/app/core/port/authentication.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthenticationFirebaseService } from '../adapter/authentication-firebase.service';
 import { EmailAlreadyTakenError } from '@app/visitor/signup/domain/email-already-taken.error';
+import { InvalidCredentialError } from '@app/visitor/login/domain/invalid-credential.error';
 
 export type RegisterResponse = RegisterPayload | EmailAlreadyTakenError;
-export type LoginResponse = LoginPayload;
+export type LoginResponse = LoginPayload | InvalidCredentialError;
 
 interface RegisterPayload {
   jwtToken: string;
@@ -32,4 +33,13 @@ export abstract class AuthenticationService {
   ): Observable<RegisterResponse>;
 
   abstract login(email: string, password: string): Observable<LoginResponse>;
+    
+  
+    /**
+   * @param refreshToken Le token de rafraîchissement (stocké localement après connexion).
+   * @returns Un Observable contenant le nouveau `jwtToken` et le `userId` de l'utilisateur.
+   */
+  abstract refreshToken(refreshToken: string): Observable<{ jwtToken: string, userId: string }>;
+
+
 }
