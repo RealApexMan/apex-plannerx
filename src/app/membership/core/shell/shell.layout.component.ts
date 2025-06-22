da3d3e79fc3466cea42fdd1cb3fa1d9f7b6866d2
RealApexMan <realapexman@gmail.com>
2025-06-22 21:41:27 +0200

    add membership routes

---
+++ src/app/membership/core/shell/shell.layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-shell',
+  imports: [RouterOutlet],
+  templateUrl: './shell.layout.component.html',
+  styleUrl: './shell.layout.component.scss'
+})
+export class ShellLayoutComponent {
+
+}
