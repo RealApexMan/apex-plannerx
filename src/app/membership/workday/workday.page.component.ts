95eca7673c147b218b7b5ce0b354291f3497756c
RealApexMan <realapexman@gmail.com>
2025-06-27 10:32:01 +0200

    Provide empty signal store and fix rules for authentication in prod

--- src/app/membership/workday/workday.page.component.ts
+++ src/app/membership/workday/workday.page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { BookSearchStore } from './workday.page.store';
 
 @Component({
   selector: 'app-workday',
   imports: [],
   templateUrl: './workday.page.component.html',
-  styleUrl: './workday.page.component.scss'
+  styleUrl: './workday.page.component.scss',
+  providers: [BookSearchStore]
 })
 export class WorkdayPageComponent {
 
